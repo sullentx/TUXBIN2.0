3be7788985edd830be0d58d850713fedd2fc5d9b
sullentx <233290@ids.upchiapas.edu.mx>
2024-11-14 20:58:15 -0600

    homeReady

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { ActivatedRoute, Router } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
